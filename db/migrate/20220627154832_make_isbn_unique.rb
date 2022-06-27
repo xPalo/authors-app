180c98a3c5ea10ad1d0e33a3225f67560e57931c
Adam Palo <xpalo@stuba.sk>
2022-06-27 17:57:30 +0200

    made isbn not unique

--- db/migrate/20220627154832_make_isbn_unique.rb
+++ db/migrate/20220627154832_make_isbn_unique.rb
@@ -1,5 +1,5 @@
 class MakeIsbnUnique < ActiveRecord::Migration[7.0]
   def change
-    add_index :books, :isbn, unique: true
+    add_index :books, :isbn
   end
 end
--- db/migrate/20220627155143_make_isbn_not_unique.rb
+++
@@ -1,5 +0,0 @@
-class MakeIsbnNotUnique < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :books, :isbn
-  end
-end
