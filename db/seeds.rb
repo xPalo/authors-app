59f3ed0b5cf37dd35cf973de6a5d406e306bbdb7
Adam Palo <xpalo@stuba.sk>
2022-07-07 11:41:00 +0200

    redesign final and database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 30.times do
+#   user = User.create! :first_name => Faker::Name.first_name,
+#                       :last_name => Faker::Name.last_name,
+#                       :email => Faker::Internet.email,
+#                       :password => 'asdasd',
+#                       :password_confirmation => 'asdasd'
+# end
+
+# 30.times do
+#   user = Author.create! :first_name => Faker::Name.first_name,
+#                         :last_name => Faker::Name.last_name,
+#                         :country => Faker::Address.country,
+#                         :user_id => rand(16..49)
+# end
+
+100.times do
+  book = Book.create! :title => Faker::Book.title,
+                      :isbn => Faker::Code.isbn,
+                      :notes => Faker::Quote.matz,
+                      :author_id => rand(39..68)
+end
